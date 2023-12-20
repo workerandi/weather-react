2de7753fdf7bcd1d83ff9d58e3c6d378f16d8f5c
workerandi <workerandiramirez@gmail.com>
2023-12-19 19:17:26 -0600

    Merged components to weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -1,24 +1,82 @@
 import "./Weather.css";
-import Leftside from "./Leftside";
-import Rightside from "./Rightside";
-import Forecast from "./Forecast";
+import axios from "axios";
+import React, { useState } from "react";
+//import Leftside from "./Leftside";
+//import Rightside from "./Rightside";
+//import Forecast from "./Forecast";
 
 export default function Weather() {
-  return (
-    <div className="WeatherApp">
-      <div className="container-fluid">
-        <div className="row">
-          <Leftside />
-          <Rightside />
+  const [ready, setReady] = useState(false);
+  const [temperature, setTemperature] = useState(null);
+
+  function handleResponse() {
+    console.log(response.data);
+    setTemperature(response.data.main.temp);
+  }
+
+  if (ready) {
+    return (
+      <div className="WeatherApp">
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col">
+              <h2 id="cityDisplayed">"Morelia"</h2>
+              <h2 id="currentTime">"adfa"</h2>
+              <br />
+              <p id="description">"asdfad"</p>
+              <img id="icon" src="sun-regular.svg" alt="icon"></img>
+
+              <form id="inputForm">
+                <input type="text" placeholder="Change City:" id="searchBar" />
+                <button type="submit" id="searchButton">
+                  Search
+                </button>
+              </form>
+            </div>
+            <div className="col">
+              <div className="mainTemp row">
+                <div className="col-6">
+                  <h1 id="temperature">{temperature}</h1>
+                </div>
+                <div className="col-6">
+                  <div className="row">
+                    <div className="col-2">
+                      <h1 className="units" id="cButton">
+                        °C
+                      </h1>
+                    </div>
+                    <div className="col-8">
+                      <h1 className="units" id="fButton">
+                        |°F
+                      </h1>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <br />
+              <p className="otherCurrentInfo" id="humidity">
+                43 %
+              </p>
+              <p className="otherCurrentInfo" id="windSpeed">
+                5 km/h
+              </p>
+            </div>
+          </div>
+          <div className="row"></div>
         </div>
-        <div className="row">
-          <Forecast />
-        </div>
+        <br />
+        <a href="https://github.com/workerandi/weather-react.git">
+          Coded by: Erandi Ramirez Oviedo
+        </a>
       </div>
-      <br />
-      <a href="https://github.com/workerandi/weather-react.git">
-        Coded by: Erandi Ramirez Oviedo
-      </a>
-    </div>
-  );
+    );
+  } else {
+    let city = "london";
+    const apiKey = "c98c59e454c889012a63b37f133be189";
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return "loading...";
+  }
 }
