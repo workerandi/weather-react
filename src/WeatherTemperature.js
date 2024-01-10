594b85b1a366a2d696cf8419662b351176382677
workerandi <workerandiramirez@gmail.com>
2024-01-09 19:18:38 -0600

    Unit Conversion Integration Completed

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,25 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function MainTemperature(props) {
-  return (
-    <div className="mainTemp row">
-      <div className="col-6 ">
-        <h1 id="temperature">{Math.round(props.celsius)}</h1>
-      </div>
-      <div className="col-6">
-        <div className="row">
-          <div className="col-4">
-            <h1 className="units" id="cButton">
-              °C
+  const [unit, setUnit] = useState("celsius");
+
+  function convert2Farenheit(event) {
+    event.preventDefault();
+    setUnit("farenheit");
+  }
+  function convert2Celsius(event) {
+    event.preventDefault();
+    setUnit("celsius");
+  }
+  function farenheit() {
+    return (props.celsius * 9) / 5 + 32;
+  }
+
+  if (unit === "celsius") {
+    return (
+      <div className="mainTemp row">
+        <div className="col-6 ">
+          <h1 id="temperature">{Math.round(props.celsius)}</h1>
+        </div>
+        <div className="col-6">
+          <div className="row">
+            <h1 className="units">
+              {" "}
+              °C |
+              <a href="/" onClick={convert2Farenheit}>
+                °F
+              </a>
             </h1>
           </div>
-          <div className="col-8">
-            <h1 className="units" id="fButton">
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="mainTemp row">
+        <div className="col-6 ">
+          <h1 id="temperature">{Math.round(farenheit())}</h1>
+        </div>
+        <div className="col-6">
+          <div className="row">
+            <h1 className="units">
+              {" "}
+              <a href="/" onClick={convert2Celsius}>
+                °C
+              </a>{" "}
               |°F
             </h1>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
